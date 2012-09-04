a557a4d7a823aaa46fc1067019f598faa0b412be
Oliver Gloth <ogloth@engits.com>
2012-09-04 22:04:52 +0200

    added entropy and density

--- src/compressiblevariables.h
+++ src/compressiblevariables.h
@@ -10,11 +10,18 @@ class CompressibleVariables
 {
 
   TGas m_Gas;
+  real m_RefPressure;
+  real m_RefTemperature;
 
 public:
 
-  int numScalars() const { return 3; }
+  CompressibleVariables();
+
+  int numScalars() const { return 5; }
   int numVectors() const { return 1; }
+
+  void setReferenceTemperature(real T) { m_RefTemperature = T; }
+  void setReferencePressure(real p)    { m_RefPressure = p; }
 
   QString getScalarName(int i) const;
   QString getVectorName(int i) const;
@@ -24,11 +31,20 @@ public:
 };
 
 template <typename TGas>
+CompressibleVariables<TGas>::CompressibleVariables()
+{
+  m_RefPressure = 1e5;
+  m_RefTemperature = 300;
+}
+
+template <typename TGas>
 QString CompressibleVariables<TGas>::getScalarName(int i) const
 {
   if (i == 0) return "Ma";
   if (i == 1) return "p";
   if (i == 2) return "T";
+  if (i == 3) return "rho";
+  if (i == 4) return "S";
   BUG;
 }
 
@@ -48,6 +64,8 @@ real CompressibleVariables<TGas>::getScalar(int i, real *var) const
   if (i == 0) return sqrt((u*u + v*v + w*w)/(m_Gas.gamma()*m_Gas.R()*T));
   if (i == 1) return p;
   if (i == 2) return T;
+  if (i == 3) return var[0];
+  if (i == 4) return TGas::cp(var)*log(T/m_RefTemperature) - TGas::R(var)*log(p/m_RefPressure);
   BUG;
 }
 
