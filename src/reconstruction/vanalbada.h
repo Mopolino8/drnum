f1d3c20fb0fce0c357e0407bb91b7c24236ddb36
Oliver Gloth <ogloth@engits.com>
2012-09-05 18:03:43 +0200

    Wikipedia version ...

--- src/reconstruction/vanalbada.h
+++ src/reconstruction/vanalbada.h
@@ -8,7 +8,7 @@ struct VanAlbada
   static real lim(real delta1, real delta2)
   {
     real r   = delta2/nonZero(delta1, global_eps);
-    return max(real(0), (2*r/(sqr(1) + 1)));
+    return (sqr(r) + r)/(sqr(r) + 1);
     //return max(real(0), (2*delta1*delta2/max(real(1e-3), sqr(delta1)+sqr(delta2))));
   }
 };
