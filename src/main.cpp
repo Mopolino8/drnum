ad9932c875d0c94fb79260ad879be94e7abd0b5d
Oliver Gloth <ogloth@engits.com>
2013-02-07 09:29:34 +0100

    working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-//#include "examples/ffs1.h"
+#include "examples/ffs1.h"
 //#include "examples/interpolation1.h"
 //#include "examples/overexpandedjet2d.h"
 //#include "examples/kelvinhelmholtz.h"
 //#include "examples/flatplate.h"
-#include "examples/wedge.h"
+//#include "examples/wedge.h"
+//#include "examples/tjunction.h"
 
 int main()
 {
