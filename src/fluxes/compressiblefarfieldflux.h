df10432a58de6211664c4aeb1b62da0af456c830
Oliver Gloth <ogloth@engits.com>
2013-06-26 12:39:38 +0200

    adapted for GPUs (not tested yet)

--- src/fluxes/compressiblefarfieldflux.h
+++ src/fluxes/compressiblefarfieldflux.h
@@ -3,36 +3,6 @@
 
 #include "compressiblewallflux.h"
 
-template <typename TReconstruction, typename TGas>
-class CompressibleFarfieldFlux
-{
-
-  real m_Var[5];
-  TReconstruction* m_Reconstruction;
-
-public:
-
-  CompressibleFarfieldFlux(TReconstruction* reconstruction);
-  void setFarfield(real p, real T, real u, real v, real w) { TGas::primitiveToConservative(p, T, u, v, w, m_Var); }
-
-  void xWallP(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-  void xWallM(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-  void yWallP(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-  void yWallM(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-  void zWallP(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-  void zWallM(CartesianPatch *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux);
-
-};
-
-
-template <typename TReconstruction, typename TGas>
-CompressibleFarfieldFlux<TReconstruction, TGas>::CompressibleFarfieldFlux(TReconstruction *reconstruction)
-{
-  m_Reconstruction = reconstruction;
-  TGas::primitiveToConservative(1e5, 300, m_Var);
-}
-
-
 #define FARFIELD_VARS \
   real p, T, u, v, w; \
   real p0, T0, u0, v0, w0; \
@@ -74,119 +44,117 @@ CompressibleFarfieldFlux<TReconstruction, TGas>::CompressibleFarfieldFlux(TRecon
     } \
   }
 
-
 template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::xWallP(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
+class CompressibleFarfieldFlux
 {
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i-1, j, k, i, j, k, x - patch->dx(), y, z, x, y, z);
-  FARFIELD_VARS
-  real M0 = -CHECKED_REAL(u0/a0);
-  real M1 = -CHECKED_REAL(u1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*u;
-  flux[1] = flux[0]*u + A*p;
-  flux[2] = flux[0]*v;
-  flux[3] = flux[0]*w;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
 
-template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::xWallM(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
-{
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i, j, k, i-1, j, k, x, y, z, x - patch->dx(), y, z);
-  FARFIELD_VARS
-  real M0 = CHECKED_REAL(u0/a0);
-  real M1 = CHECKED_REAL(u1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*u;
-  flux[1] = flux[0]*u + A*p;
-  flux[2] = flux[0]*v;
-  flux[3] = flux[0]*w;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
+  real m_Var[5];
+  TReconstruction m_Reconstruction;
 
-template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::yWallP(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
-{
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i, j-1, k, i, j, k, x, y - patch->dy(), z, x, y, z);
-  FARFIELD_VARS
-  real M0 = -CHECKED_REAL(v0/a0);
-  real M1 = -CHECKED_REAL(v1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*v;
-  flux[1] = flux[0]*u;
-  flux[2] = flux[0]*v + A*p;
-  flux[3] = flux[0]*w;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
+public:
 
-template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::yWallM(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
-{
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i, j, k, i, j-1, k, x, y, z, x, y - patch->dy(), z);
-  FARFIELD_VARS
-  real M0 = CHECKED_REAL(v0/a0);
-  real M1 = CHECKED_REAL(v1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*v;
-  flux[1] = flux[0]*u;
-  flux[2] = flux[0]*v + A*p;
-  flux[3] = flux[0]*w;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
+  CompressibleFarfieldFlux()
+  {
+    TGas::primitiveToConservative(1e5, 300, 0, 0, 0, m_Var);
+  }
 
-template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::zWallP(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
-{
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i, j, k-1, i, j, k, x, y, z - patch->dz(), x, y, z);
-  FARFIELD_VARS
-  real M0 = -CHECKED_REAL(w0/a0);
-  real M1 = -CHECKED_REAL(w1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*w;
-  flux[1] = flux[0]*u;
-  flux[2] = flux[0]*v;
-  flux[3] = flux[0]*w + A*p;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
+  void setFarfield(real p, real T, real u, real v, real w)
+  {
+    TGas::primitiveToConservative(p, T, u, v, w, m_Var);
+  }
 
-template <typename TReconstruction, typename TGas>
-void CompressibleFarfieldFlux<TReconstruction, TGas>::zWallM(CartesianPatch *patch,
-                                                             size_t i, size_t j, size_t k,
-                                                             real x, real y, real z,
-                                                             real A, real *flux)
-{
-  real var1[5];
-  m_Reconstruction->project(patch, var1, 0, 5, i, j, k, i, j, k-1, x, y, z, x, y, z - patch->dz());
-  FARFIELD_VARS
-  real M0 = CHECKED_REAL(w0/a0);
-  real M1 = CHECKED_REAL(w1/a1);
-  FARFIELD_SPLIT
-  flux[0] = A*p/(TGas::R()*T)*w;
-  flux[1] = flux[0]*u;
-  flux[2] = flux[0]*v;
-  flux[3] = flux[0]*w + A*p;
-  flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
-}
+  template <typename PATCH> CUDA_DH void xWallP(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i-1, j, k, i, j, k, x - patch->dx(), y, z, x, y, z);
+    FARFIELD_VARS
+    real M0 = -CHECKED_REAL(u0/a0);
+    real M1 = -CHECKED_REAL(u1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*u;
+    flux[1] = flux[0]*u + A*p;
+    flux[2] = flux[0]*v;
+    flux[3] = flux[0]*w;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+  template <typename PATCH> CUDA_DH void xWallM(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i, j, k, i-1, j, k, x, y, z, x - patch->dx(), y, z);
+    FARFIELD_VARS
+    real M0 = CHECKED_REAL(u0/a0);
+    real M1 = CHECKED_REAL(u1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*u;
+    flux[1] = flux[0]*u + A*p;
+    flux[2] = flux[0]*v;
+    flux[3] = flux[0]*w;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+  template <typename PATCH> CUDA_DH void yWallP(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i, j-1, k, i, j, k, x, y - patch->dy(), z, x, y, z);
+    FARFIELD_VARS
+    real M0 = -CHECKED_REAL(v0/a0);
+    real M1 = -CHECKED_REAL(v1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*v;
+    flux[1] = flux[0]*u;
+    flux[2] = flux[0]*v + A*p;
+    flux[3] = flux[0]*w;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+  template <typename PATCH> CUDA_DH void yWallM(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i, j, k, i, j-1, k, x, y, z, x, y - patch->dy(), z);
+    FARFIELD_VARS
+    real M0 = CHECKED_REAL(v0/a0);
+    real M1 = CHECKED_REAL(v1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*v;
+    flux[1] = flux[0]*u;
+    flux[2] = flux[0]*v + A*p;
+    flux[3] = flux[0]*w;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+  template <typename PATCH> CUDA_DH void zWallP(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i, j, k-1, i, j, k, x, y, z - patch->dz(), x, y, z);
+    FARFIELD_VARS
+    real M0 = -CHECKED_REAL(w0/a0);
+    real M1 = -CHECKED_REAL(w1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*w;
+    flux[1] = flux[0]*u;
+    flux[2] = flux[0]*v;
+    flux[3] = flux[0]*w + A*p;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+  template <typename PATCH> CUDA_DH void zWallM(PATCH *patch, size_t i, size_t j, size_t k, real x, real y, real z, real A, real* flux)
+  {
+    real var1[5];
+    m_Reconstruction.project(patch, var1, 0, 5, i, j, k, i, j, k-1, x, y, z, x, y, z - patch->dz());
+    FARFIELD_VARS
+    real M0 = CHECKED_REAL(w0/a0);
+    real M1 = CHECKED_REAL(w1/a1);
+    FARFIELD_SPLIT
+    flux[0] = A*p/(TGas::R()*T)*w;
+    flux[1] = flux[0]*u;
+    flux[2] = flux[0]*v;
+    flux[3] = flux[0]*w + A*p;
+    flux[4] = flux[0]*(TGas::cp()*T + 0.5*(u*u + v*v + w*w));
+  }
+
+};
+
+
 
 #endif // COMPRESSIBLEFARFIELDFLUX_H
