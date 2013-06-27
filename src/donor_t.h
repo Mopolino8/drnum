02345f779d20060f428f696c2e4f3b0b6f7a75c7
Oliver Gloth <ogloth@engits.com>
2013-06-27 11:14:14 +0200

    mat3_t is not working on the GPU (at least not "out of the box")

--- src/donor_t.h
+++ src/donor_t.h
@@ -13,7 +13,15 @@ struct donor_t
   size_t stride;                     ///< Fixed number of donor cells for each receiving cell
   size_t receiver_index_field_start; ///< Starting index in concatenated receiving cell indicees field of receiving patch
   size_t donor_wi_field_start;       ///< Starting index in concatenated index and weight field for all donor patches
-  mat3_t transformation;             ///< transformation matrix from donor to receiver
+  real   transform_xx;               ///< xx component of transformation matrix from donor to receiver
+  real   transform_xy;               ///< xy component of transformation matrix from donor to receiver
+  real   transform_xz;               ///< xz component of transformation matrix from donor to receiver
+  real   transform_yx;               ///< yx component of transformation matrix from donor to receiver
+  real   transform_yy;               ///< yy component of transformation matrix from donor to receiver
+  real   transform_yz;               ///< yz component of transformation matrix from donor to receiver
+  real   transform_zx;               ///< zx component of transformation matrix from donor to receiver
+  real   transform_zy;               ///< zy component of transformation matrix from donor to receiver
+  real   transform_zz;               ///< zz component of transformation matrix from donor to receiver
 };
 
 #endif // DONOR_T_H
