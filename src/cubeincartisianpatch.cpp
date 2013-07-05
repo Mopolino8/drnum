9f08cea3e23cc23a113cb208330822ab28d28a8b
Oliver Gloth <ogloth@engits.com>
2013-07-05 20:07:07 +0200

    can stick in and out of patch now

--- src/cubeincartisianpatch.cpp
+++ src/cubeincartisianpatch.cpp
@@ -45,9 +45,9 @@ void CubeInCartisianPatch::setRange(vec3_t x1, vec3_t x2)
     for (size_t j = 0; j < m_Patch->sizeJ(); ++j) {
       for (size_t i = 0; i < m_Patch->sizeI(); ++i) {
         vec3_t x;
-        x[0] = i * m_Patch->dx();
-        x[1] = j * m_Patch->dy();
-        x[2] = k * m_Patch->dz();
+        x[0] = (real(i) + 0.5)*m_Patch->dx();
+        x[1] = (real(j) + 0.5)*m_Patch->dy();
+        x[2] = (real(k) + 0.5)*m_Patch->dz();
         x = m_Patch->getTransformInertial2This().transformReverse(x);
         if (x[0] > x1[0] && x[1] > x1[1] && x[2] > x1[2]) {
           if (x[0] < x2[0] && x[1] < x2[1] && x[2] < x2[2]) {
@@ -90,44 +90,60 @@ void CubeInCartisianPatch::operator ()()
 
     size3_t start = m_Start;
     size3_t stop = m_Stop;
-    start.i += layer;
-    start.j += layer;
-    start.k += layer;
-    stop.i -= layer;
-    stop.j -= layer;
-    stop.k -= layer;
+    if (m_Start.i > 0) {
+      start.i += layer;
+    }
+    if (m_Start.j > 0) {
+      start.j += layer;
+    }
+    if (m_Start.k > 0) {
+      start.k += layer;
+    }
+    if (m_Stop.i < m_Patch->sizeI() - 1) {
+      stop.i -= layer;
+    }
+    if (m_Stop.j < m_Patch->sizeJ() - 1) {
+      stop.j -= layer;
+    }
+    if (m_Stop.k < m_Patch->sizeK() - 1) {
+      stop.k -= layer;
+    }
 
     // K planes
     for (size_t i = start.i; i < stop.i; ++i) {
       for (size_t j = start.j; j < stop.j; ++j) {
-        m_Patch->getVar(0, i, j, start.k - 1, var1);
-        m_Patch->getVar(0, i, j, start.k, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(i, j, start.k);
-        m_Patch->setVar(0, i, j, start.k, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
+        if (m_Start.k > 0) {
+          m_Patch->getVar(0, i, j, start.k - 1, var1);
+          m_Patch->getVar(0, i, j, start.k, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(i, j, start.k);
+          m_Patch->setVar(0, i, j, start.k, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
+          }
         }
 
-        m_Patch->getVar(0, i, j, stop.k, var1);
-        m_Patch->getVar(0, i, j, stop.k - 1, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(i, j, stop.k - 1);
-        m_Patch->setVar(0, i, j, stop.k - 1, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
+        if (m_Stop.k < m_Patch->sizeK() - 1) {
+          m_Patch->getVar(0, i, j, stop.k, var1);
+          m_Patch->getVar(0, i, j, stop.k - 1, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(i, j, stop.k - 1);
+          m_Patch->setVar(0, i, j, stop.k - 1, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
+          }
         }
       }
     }
@@ -135,43 +151,38 @@ void CubeInCartisianPatch::operator ()()
     // I planes
     for (size_t j = start.j; j < stop.j; ++j) {
       for (size_t k = start.k; k < stop.k; ++k) {
-        real weight = 1.0;
-        if (j == start.j || j == stop.j - 1) {
-          if (k == start.k || k == stop.k - 1) {
-            weight = 1.0/3.0;
-          } else {
-            weight = 0.5;
+        if (m_Start.i > 0) {
+          m_Patch->getVar(0, start.i - 1, j, k, var1);
+          m_Patch->getVar(0, start.i, j, k, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(start.i, j, k);
+          m_Patch->setVar(0, start.i, j, k, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
           }
         }
 
-        m_Patch->getVar(0, start.i - 1, j, k, var1);
-        m_Patch->getVar(0, start.i, j, k, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(start.i, j, k);
-        m_Patch->setVar(0, start.i, j, k, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
-        }
-
-        m_Patch->getVar(0, stop.i, j, k, var1);
-        m_Patch->getVar(0, stop.i - 1, j, k, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(stop.i - 1, j, k);
-        m_Patch->setVar(0, stop.i - 1, j, k, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
+        if (m_Stop.i < m_Patch->sizeI() - 1) {
+          m_Patch->getVar(0, stop.i, j, k, var1);
+          m_Patch->getVar(0, stop.i - 1, j, k, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(stop.i - 1, j, k);
+          m_Patch->setVar(0, stop.i - 1, j, k, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
+          }
         }
       }
     }
@@ -179,43 +190,38 @@ void CubeInCartisianPatch::operator ()()
     // J planes
     for (size_t i = start.i; i < stop.i; ++i) {
       for (size_t k = start.k; k < stop.k; ++k) {
-        real weight = 1.0;
-        if (i == start.i || i == stop.i - 1) {
-          if (k == start.k || k == stop.k - 1) {
-            weight = 1.0/3.0;
-          } else {
-            weight = 0.5;
+        if (m_Start.j > 0) {
+          m_Patch->getVar(0, i, start.j - 1, k, var1);
+          m_Patch->getVar(0, i, start.j, k, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(i, start.j, k);
+          m_Patch->setVar(0, i, start.j, k, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
           }
         }
 
-        m_Patch->getVar(0, i, start.j - 1, k, var1);
-        m_Patch->getVar(0, i, start.j, k, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(i, start.j, k);
-        m_Patch->setVar(0, i, start.j, k, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
-        }
-
-        m_Patch->getVar(0, i, stop.j, k, var1);
-        m_Patch->getVar(0, i, stop.j - 1, k, var2);
-        PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
-        PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
-        for (int i_var = 0; i_var < 5; ++i_var) {
-          var2[i_var] += var1[i_var];
-        }
-        count(i, stop.j - 1, k);
-        m_Patch->setVar(0, i, stop.j - 1, k, var2);
-        if (layer == 0) {
-          ++N;
-          p0 += p;
-          T0 += T;
+        if (m_Stop.j < m_Patch->sizeJ() - 1) {
+          m_Patch->getVar(0, i, stop.j, k, var1);
+          m_Patch->getVar(0, i, stop.j - 1, k, var2);
+          PerfectGas::conservativeToPrimitive(var1, p, T, u, v, w);
+          PerfectGas::primitiveToConservative(p, T, 0, 0, 0, var1);
+          for (int i_var = 0; i_var < 5; ++i_var) {
+            var2[i_var] += var1[i_var];
+          }
+          count(i, stop.j - 1, k);
+          m_Patch->setVar(0, i, stop.j - 1, k, var2);
+          if (layer == 0) {
+            ++N;
+            p0 += p;
+            T0 += T;
+          }
         }
       }
     }
@@ -238,11 +244,33 @@ void CubeInCartisianPatch::operator ()()
 
   p0 /= N;
   T0 /= N;
-  PerfectGas::primitiveToConservative(p0, T0, 0, 0, 0, var1);
-  for (size_t i = m_Start.i + m_NumLayers; i < m_Stop.i - m_NumLayers; ++i) {
-    for (size_t j = m_Start.j + m_NumLayers; j < m_Stop.j - m_NumLayers; ++j) {
-      for (size_t k = m_Start.k + m_NumLayers; k < m_Stop.k - m_NumLayers; ++k) {
-        m_Patch->setVar(0, i, j, k, var1);
+  {
+    size3_t start = m_Start;
+    size3_t stop = m_Stop;
+    if (m_Start.i > 0) {
+      start.i += m_NumLayers;
+    }
+    if (m_Start.j > 0) {
+      start.j += m_NumLayers;
+    }
+    if (m_Start.k > 0) {
+      start.k += m_NumLayers;
+    }
+    if (m_Stop.i < m_Patch->sizeI() - 1) {
+      stop.i -= m_NumLayers;
+    }
+    if (m_Stop.j < m_Patch->sizeJ() - 1) {
+      stop.j -= m_NumLayers;
+    }
+    if (m_Stop.k < m_Patch->sizeK() - 1) {
+      stop.k -= m_NumLayers;
+    }
+    PerfectGas::primitiveToConservative(p0, T0, 0, 0, 0, var1);
+    for (size_t i = start.i; i < stop.i; ++i) {
+      for (size_t j = start.j; j < stop.j; ++j) {
+        for (size_t k = start.k; k < stop.k; ++k) {
+          m_Patch->setVar(0, i, j, k, var1);
+        }
       }
     }
   }
