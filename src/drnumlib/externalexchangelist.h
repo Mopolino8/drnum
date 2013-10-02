03b3f0850d89c5ed9787b08c475e07c62c1631ea
Oliver Gloth <ogloth@engits.com>
2013-10-02 18:50:33 +0200

    added sync functionality

--- src/drnumlib/externalexchangelist.h
+++ src/drnumlib/externalexchangelist.h
@@ -18,15 +18,66 @@
 // + along with DrNUM. If not, see <http://www.gnu.org/licenses/>.        +
 // +                                                                      +
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 #ifndef EXTERNALEXCHANGELIST_H
 #define EXTERNALEXCHANGELIST_H
+
+#include <vector>
+#include <list>
+
+#include "mpicommunicator.h""
 
 class ExternalExchangeList
 {
 
+
+public: // data types
+
+  struct cell_t
+  {
+    int grid, index;
+    bool operator<(const cell_t &cell);
+  };
+
+  struct xyz_cell_t
+  {
+    int grid, index;
+    double x, y, z;
+    bool operator<(const xyz_cell_t &cell);
+  };
+
+
+private: // attributes
+
+  std::list<xyz_cell_t> m_XyzCells;
+  std::list<cell_t>     m_Cells;
+  MpiCommunicator      *m_MpiComm;
+  bool                  m_Finalised;
+
+  std::list<xyz_cell_t>::iterator m_XyzIter;
+  std::list<cell_t>::iterator     m_Iter;
+
+
 public:
 
-  ExternalExchangeList();
+  ExternalExchangeList(MpiCommunicator *mpi_comm);
+
+  void   addCell(int grid, int index, double x, double y, double z);
+  int    grid();
+  int    index();
+  double x();
+  double y();
+  double z();
+  void   send();
+  void   receive(int rank);
+  void   operator+=(const ExternalExchangeList &exchange_list);
+  void   append(int grid, int index, double x, double y, double z);
+  void   sort();
+  void   finalise();
+  int    size();
+  void   initIteration();
+  void   nextCell();
+  bool   endCell();
 
 };
 
