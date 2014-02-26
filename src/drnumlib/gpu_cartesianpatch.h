352e530ac408e7500838854de80b4d521b511eaf
Oliver Gloth <ogloth@herring.todtnau.engits.de>
2014-02-26 15:29:34 +0100

    implemented partial copy from device (needs more debugging)

--- src/drnumlib/gpu_cartesianpatch.h
+++ src/drnumlib/gpu_cartesianpatch.h
@@ -27,6 +27,14 @@
 #include "cartesianpatch.h"
 #include "gpu_patch.h"
 
+class GPU_CartesianPatch;
+
+template <unsigned int DIM>
+__global__ void GPU_CartesianPatch_kernelPartialCopy(GPU_CartesianPatch patch, size_t i_field, real *tmp_array,
+                                                     size_t i1, size_t i2,
+                                                     size_t j1, size_t j2,
+                                                     size_t k1, size_t k2);
+
 class GPU_CartesianPatch : public GPU_Patch
 {
 
@@ -41,7 +49,62 @@ public:
     copyAttributes(patch);
   }
 
+  template <unsigned int DIM>
+  CUDA_HO void partialCopyFromDevice(CartesianPatch *patch, size_t i_field,
+                                     size_t i1, size_t i2,
+                                     size_t j1, size_t j2,
+                                     size_t k1, size_t k2)
+  {
+    CUDA_CHECK_ERROR
+    size_t dim = patch->numVariables();
+    size_t tmp_array_length = DIM*(i2-i1)*(j2-j1)*(k2-k1);
+    real *tmp_device_array, *tmp_host_array;
+    cudaThreadSynchronize();
+    cudaMalloc(&tmp_device_array, tmp_array_length*sizeof(real));
+    CUDA_CHECK_ERROR
+    tmp_host_array = new real[tmp_array_length];
+    GPU_CartesianPatch_kernelPartialCopy <DIM> <<<1, 1>>> (*this, i_field, tmp_device_array, i1, i2, j1, j2, k1, k2);
+    cudaThreadSynchronize();
+    CUDA_CHECK_ERROR
+    cudaMemcpy(tmp_host_array, tmp_device_array, tmp_array_length, cudaMemcpyDeviceToHost);
+    CUDA_CHECK_ERROR
+    size_t i_tmp = 0;
+    for (size_t i_var = 0; i_var < dim; ++i_var) {
+      for (size_t i = i1; i < i2; ++i) {
+        for (size_t j = j1; j < j2; ++j) {
+          for (size_t k = k1; k < k2; ++k) {
+            patch->f(i_field, i_var, i, j, k) = tmp_host_array[i_tmp];
+            ++i_tmp;
+          }
+        }
+      }
+    }
+    cudaFree(tmp_device_array);
+    CUDA_CHECK_ERROR
+    delete [] tmp_host_array;
+  }
+
 };
+
+
+template <unsigned int DIM>
+__global__ void GPU_CartesianPatch_kernelPartialCopy(GPU_CartesianPatch patch, size_t i_field, real *tmp_array,
+                                                     size_t i1, size_t i2,
+                                                     size_t j1, size_t j2,
+                                                     size_t k1, size_t k2)
+{
+  size_t i_tmp = 0;
+  for (size_t i_var = 0; i_var < DIM; ++i_var) {
+    for (size_t i = i1; i < i2; ++i) {
+      for (size_t j = j1; j < j2; ++j) {
+        for (size_t k = k1; k < k2; ++k) {
+          tmp_array[i_tmp] = patch.f(i_field, i_var, i, j, k);
+          ++i_tmp;
+        }
+      }
+    }
+  }
+}
 
 
 
