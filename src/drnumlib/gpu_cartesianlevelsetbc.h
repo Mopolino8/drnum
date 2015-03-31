38c6aaf443ff325301718c118e001e00023bcf7e
Oliver Gloth <ogloth@engits.com>
2015-03-31 12:35:59 +0200

    skeleton for template level set BC

---
+++ src/drnumlib/gpu_cartesianlevelsetbc.h
@@ -0,0 +1,41 @@
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +                                                                      +
+// + This file is part of DrNUM.                                          +
+// +                                                                      +
+// + Copyright 2013 numrax GmbH, enGits GmbH                              +
+// +                                                                      +
+// + DrNUM is free software: you can redistribute it and/or modify        +
+// + it under the terms of the GNU General Public License as published by +
+// + the Free Software Foundation, either version 3 of the License, or    +
+// + (at your option) any later version.                                  +
+// +                                                                      +
+// + DrNUM is distributed in the hope that it will be useful,             +
+// + but WITHOUT ANY WARRANTY; without even the implied warranty of       +
+// + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        +
+// + GNU General Public License for more details.                         +
+// +                                                                      +
+// + You should have received a copy of the GNU General Public License    +
+// + along with DrNUM. If not, see <http://www.gnu.org/licenses/>.        +
+// +                                                                      +
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#ifndef GPU_LEVELSETBC_H
+#define GPU_LEVELSETBC_H
+
+template <unsigned int DIM, typename BC>
+class GPU_CylinderInCartisianPatch;
+
+#include "cudatools.h"
+
+#include "perfectgas.h"
+
+#include "drnum.h"
+#include "genericoperation.h"
+
+template <unsigned int DIM, typename BC>
+class GPU_LevelSetBC : public GenericOperation
+{
+
+};
+
+#endif // GPU_LEVELSETBC_H
